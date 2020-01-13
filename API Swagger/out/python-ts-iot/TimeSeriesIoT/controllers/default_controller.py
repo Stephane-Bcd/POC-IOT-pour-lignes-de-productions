0b9dbc5475acb4e2f699a221c2526bda847c2757
Stephane Bcd <stephane.boucaud@outlook.fr>
2020-01-13 17:42:31 +0100

    configured swagger API

--- API Swagger/out/python-ts-iot/TimeSeriesIoT/controllers/default_controller.py
+++ API Swagger/out/python-ts-iot/TimeSeriesIoT/controllers/default_controller.py
@@ -3,6 +3,105 @@ import six
 
 from TimeSeriesIoT import util
 
+import pymongo
+from bson.json_util import dumps
+from datetime import datetime
+
+#docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongodb_mongo_1
+
+def connect_to_collection(address="172.19.0.4", port="27017", db="Client1", col="captures"):
+    myclient = pymongo.MongoClient("mongodb://"+address+":"+port+"/")
+    mydb = myclient[db]
+    mycol = mydb[col]
+    return mycol
+
+def get_sensor_captures(mycol=connect_to_collection(), sensor_name="Temperature0"):
+    myquery = {"NomCapteur": sensor_name}
+
+    mydoc = mycol.find(myquery).sort("TimestampCapture", 1) #tri en descendant
+    
+    return dumps(mydoc)
+
+def get_last_sensor_capture(mycol=connect_to_collection(), sensor_name="Temperature0"):
+    myquery = {"NomCapteur": sensor_name}
+
+    mydoc = mycol.find(myquery).sort("TimestampCapture", -1) #tri en descendant
+    
+    return dumps(mydoc[0])
+
+def get_avg_in_period(mycol=connect_to_collection(), sensor_name="Temperature0", start=datetime(2019, 9, 1, 00, 00, 00), end=datetime(2020, 1, 1, 00, 00, 00)):
+
+    start_str = start.strftime("%d/%m/%Y %H:%M:%S")
+    end_str = end.strftime("%d/%m/%Y %H:%M:%S")
+    start = start.timestamp()
+    end = end.timestamp()
+
+    pipeline = [
+        {
+            "$match": {
+                "NomCapteur": sensor_name,
+                "TimestampCapture": {
+                    "$gte": start,
+                    "$lt": end
+                }  
+            }
+        },
+        {
+            "$group": {
+                "_id": "$NomCapteur",
+                "average": {
+                    "$avg": "$ValeurCapture"
+                }
+            }
+        },
+        {"$sort": {"TimestampCapture": -1}}
+    ]
+
+
+    mydoc = mycol.aggregate(pipeline)
+
+    return dumps(mydoc)
+
+def get_min_in_period(mycol=connect_to_collection(), sensor_name="Temperature0", start=datetime(2018, 9, 1, 00, 00, 00), end=datetime(2020, 1, 1, 00, 00, 00)):
+
+    start_str = start.strftime("%d/%m/%Y %H:%M:%S")
+    end_str = end.strftime("%d/%m/%Y %H:%M:%S")
+    start = start.timestamp()
+    end = end.timestamp()
+
+    pipeline = [
+        {
+            "$match": {
+                "NomCapteur": sensor_name,
+                "TimestampCapture": {
+                    "$gte": start,
+                    "$lt": end
+                }  
+            }
+        },
+        {
+            "$group": {
+                "_id": "$NomCapteur",
+                "min": {
+                    "$min": "$ValeurCapture"
+                }
+            }
+        },
+        {"$sort": {"TimestampCapture": -1}}
+    ]
+
+
+    mydoc = mycol.aggregate(pipeline)
+ 
+    return dumps(mydoc)
+
+def create_indexes(col=connect_to_collection()):
+    resp = col.create_index([ 
+        ("TimestampCapture", -1),
+        ("NomCapteur", -1),
+        ("DateCapture", -1)
+    ])
+
 
 def allrecords_get():  # noqa: E501
     """Retourne toutes les captures des capteurs
@@ -12,7 +111,7 @@ def allrecords_get():  # noqa: E501
 
     :rtype: List[int]
     """
-    return 'do some magic!'
+    return get_sensor_captures()
 
 
 def last_sensor_id_get(sensor_id):  # noqa: E501
@@ -25,27 +124,32 @@ def last_sensor_id_get(sensor_id):  # noqa: E501
 
     :rtype: List[int]
     """
-    return 'do some magic!'
+    return get_last_sensor_capture(sensor_name=sensor_id)
 
 
-def mean_sensor_id_get(sensor_id, start_date=None, end_date=None):  # noqa: E501
-    """Calculer la moyenne d&#x27;un capteur entre deux dates
+def mean_sensor_id_get(sensor_id, start_date=1484275868, end_date=1578883868 ):  # noqa: E501
+	"""Calculer la moyenne d&#x27;un capteur entre deux dates
 
-    Optional extended description in CommonMark or HTML. # noqa: E501
+	Optional extended description in CommonMark or HTML. # noqa: E501
 
-    :param sensor_id: String Id of the sensor to get
-    :type sensor_id: str
-    :param start_date: Integer/timestamp of the start date
-    :type start_date: int
-    :param end_date: Integer/timestamp of the end date
-    :type end_date: int
+	:param sensor_id: String Id of the sensor to get
+	:type sensor_id: str
+	:param start_date: Integer/timestamp of the start date
+	:type start_date: int
+	:param end_date: Integer/timestamp of the end date
+	:type end_date: int
 
-    :rtype: List[int]
-    """
-    return 'do some magic!'
+	:rtype: List[int]
+	"""
+
+	#create_indexes()
+
+	return get_avg_in_period(sensor_name=sensor_id, start=datetime.fromtimestamp(start_date), end=datetime.fromtimestamp(end_date))
+
+#print(mean_sensor_id_get(sensor_id="Temperature3"))
 
 
-def min_sensor_id_get(sensor_id, start_date=None, end_date=None):  # noqa: E501
+def min_sensor_id_get(sensor_id, start_date=1484275868, end_date=1578883868):  # noqa: E501
     """Calculer la valeur minimale d&#x27;un capteur entre deux dates
 
     Optional extended description in CommonMark or HTML. # noqa: E501
@@ -59,4 +163,4 @@ def min_sensor_id_get(sensor_id, start_date=None, end_date=None):  # noqa: E501
 
     :rtype: List[int]
     """
-    return 'do some magic!'
+    return get_min_in_period(sensor_name=sensor_id, start=datetime.fromtimestamp(start_date), end=datetime.fromtimestamp(end_date))
